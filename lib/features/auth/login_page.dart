1a35b705d239aba7d47b35840922d8d0df47cc96
Ramzscuyyy <1123150076@global.ac.id>
2026-04-23 21:44:42 +0700

    perbaiki bckend

--- lib/features/auth/login_page.dart
+++ lib/features/auth/login_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'register_page.dart';
 import '../catalog/home_page.dart';
+import '../../../core/api_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -15,37 +17,64 @@ class _LoginPageState extends State<LoginPage> {
   final password = TextEditingController();
 
   Future login() async {
-    try {
-      final userCredential =
-          await FirebaseAuth.instance
-              .signInWithEmailAndPassword(
-        email: email.text.trim(),
-        password: password.text.trim(),
+  try {
+    final userCredential =
+        await FirebaseAuth.instance
+            .signInWithEmailAndPassword(
+      email: email.text.trim(),
+      password: password.text.trim(),
+    );
+
+    final user = userCredential.user;
+
+    if (user != null &&
+        user.emailVerified) {
+
+      final firebaseToken =
+          await user.getIdToken();
+
+      final jwt =
+          await ApiService.loginBackend(
+        firebaseToken!,
+        user.email!,
       );
 
-      if (userCredential.user != null &&
-          userCredential.user!.emailVerified) {
+      if (jwt != null) {
+        final prefs =
+            await SharedPreferences
+                .getInstance();
+
+        await prefs.setString(
+            "jwt", jwt);
+
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-            builder: (_) => const HomePage(),
-          ),
-        );
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text("Verify email first"),
+            builder: (_) =>
+                const HomePage(),
           ),
         );
       }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
+
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(
         const SnackBar(
-          content: Text("Login Failed"),
+          content: Text(
+              "Verify email first"),
         ),
       );
     }
+  } catch (e) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(
+      const SnackBar(
+        content:
+            Text("Login Failed"),
+      ),
+    );
   }
+}
 
   @override
   Widget build(BuildContext context) {
