87be57c9c875ccad1f1866caff09419e7143449b
Ramzscuyyy <1123150076@global.ac.id>
2026-04-23 20:04:29 +0700

    cart provide

---
+++ lib/cart_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CartProvider extends ChangeNotifier {
+  final List<Map<String, dynamic>> cart = [];
+
+  void addItem(Map<String, dynamic> item) {
+    cart.add(item);
+    notifyListeners();
+  }
+
+  void removeItem(int index) {
+    cart.removeAt(index);
+    notifyListeners();
+  }
+
+  int get totalPrice {
+    int total = 0;
+    for (var item in cart) {
+      total += item["price"] as int;
+    }
+    return total;
+  }
+
+  int get totalItem => cart.length;
+}
