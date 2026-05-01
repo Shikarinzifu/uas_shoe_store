97cb0b65e794f927077bf1dad108333801832492
Ramzscuyyy <1123150076@global.ac.id>
2026-05-01 15:14:41 +0700

    memperbaiki kesalahan

--- lib/features/auth/register_page.dart
+++ lib/features/auth/register_page.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final email = TextEditingController();
+  final password = TextEditingController();
+
+  Future register() async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email.text.trim(),
+        password: password.text.trim(),
+      );
+
+      await userCredential.user!.sendEmailVerification();
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Register success, check email")),
+      );
+
+      Navigator.pop(context);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Register Failed")),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +40,26 @@ class RegisterPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Register"),
       ),
-      body: const Center(
-        child: Text("Register Page"),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: email,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: password,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: "Password"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: register,
+              child: const Text("Register"),
+            ),
+          ],
+        ),
       ),
     );
   }
